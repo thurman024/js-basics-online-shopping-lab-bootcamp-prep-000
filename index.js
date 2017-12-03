43c2a0a0ca70f12e830c265c0274e3e227513606
Stephen Thurman <thurman024@gmail.com>
2017-12-03 16:36:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.floor(Math.random()*100)
+ cart.push({[item]: itemPrice})
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length == 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  else if (cart.length ==1) {
+    var itemName = Object.keys(cart[0])
+    console.log(`In your cart, you have ${itemName} at $${cart[0][itemName]}.`)
+  }
+  else if (cart.length == 2) {
+    var items = []
+    for (let i=0;i<2;i++){
+      items[i] = Object.keys(cart[i])[0]
+    }
+    console.log(`In your cart, you have ${items[0]} at $${cart[0][items[0]]} and ${items[1]} at $${cart[0][items[1]]}.`)
+  }
 }
 
 function total() {
